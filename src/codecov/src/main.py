7cd8d5c9780ecd2258e3e0a69229fb0312d942b9
Toshitaka Satomi <satomi.toshitak@fujitsu.com>
2022-08-25 10:45:55 +0900

    Modify the codecov test python scripts

--- src/codecov/src/main.py
+++ src/codecov/src/main.py
@@ -2,22 +2,22 @@
 
 def func_1(a_int: int) -> int:
     """ func_1 """
-    #print(f'Enter func_1 ({a_int})')
+    print(f'Enter func_1 ({a_int})')
     return a_int
 
 def func_2(a_int: int) -> int:
     """ func_2 """
-    #print(f'Enter func_2 ({a_int})')
+    print(f'Enter func_2 ({a_int})')
     return a_int
 
-def main(cond_1: bool,cond_2: bool) -> None:
-    """ main """
-    #print(f'Enter main (cond_1: {cond_1})')
-    #print(f'Enter main (cond_2: {cond_2})')
-    if cond_1:
-        func_1(10)
-    if cond_2:
-        func_2(20)
+#def main(cond_1: bool,cond_2: bool) -> None:
+#    """ main """
+#    #print(f'Enter main (cond_1: {cond_1})')
+#    #print(f'Enter main (cond_2: {cond_2})')
+#    if cond_1:
+#        func_1(10)
+#    if cond_2:
+#        func_2(20)
 
-if __name__ ==  '__main__':
-    main(False, True)
+#if __name__ ==  '__main__':
+#    main(False, True)
--- src/codecov/test/test_main.py
+++ src/codecov/test/test_main.py
@@ -11,22 +11,18 @@ class TestMethods(unittest.TestCase):
     def tearDown(self) -> None:
         pass
 
-    def test_func_1_case_1(self) -> None:
+    def test_func_1_success(self) -> None:
         """ func_1, success """
         self.assertEqual(main.func_1(10), 10)
 
-    def test_func_1_case_2(self) -> None:
-        """ func_1, fail """
-        self.assertEqual(main.func_1(10), 10)
+    #def test_func_1_fail(self) -> None:
+    #    """ func_1, fail """
+    #    self.assertEqual(main.func_1(10), 20)
 
-    #def test_func_2_case_1(self) -> None:
-    #    """ func_2, success """
-    #    self.assertEqual(main.func_2(10), 10)
+    def test_func_2_success(self) -> None:
+        """ func_2, success """
+        self.assertEqual(main.func_2(10), 10)
 
-    #def test_func_2_case_2(self) -> None:
+    #def test_func_2_fail(self) -> None:
     #    """ func_2, fail """
-    #    self.assertEqual(main.func_2(10), 10)
-
-if __name__ == '__main__':
-    runner = unittest.TextTestRunner()
-    runner.run(TestMethods)
+    #    self.assertEqual(main.func_2(10), 20)
