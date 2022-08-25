45a68e81455a6e425c6216ed31ca241f6e1c09c1
Toshitaka Satomi <satomi.toshitak@fujitsu.com>
2022-08-25 11:26:20 +0900

    Move and rename the test files

---
+++ src/codecov/package/package_class.py
@@ -0,0 +1,18 @@
+""" Test for main module """
+
+class PackageClass (object):
+    """ PakcageClass """
+
+    def __init__(self) -> None:
+        """ dummy __init__ """
+        return
+
+    def func_1(self, a_int: int) -> int:
+        """ func_1 """
+        print(f'Enter func_1 ({a_int})')
+        return a_int
+
+    def func_2(self, a_int: int) -> int:
+        """ func_2 """
+        print(f'Enter func_2 ({a_int})')
+        return a_int
--- src/codecov/test/test_package_class.py
+++ src/codecov/test/test_package_class.py
@@ -1,28 +1,28 @@
 """ test.py for main.py"""
 import unittest
-from codecov.src import main
+from codecov.package.package_class import PackageClass
 
 
 class TestMethods(unittest.TestCase):
     """ Test Class"""
     def setUp(self) -> None:
-        pass
+        self.pc = PackageClass()
 
     def tearDown(self) -> None:
         pass
 
     def test_func_1_success(self) -> None:
         """ func_1, success """
-        self.assertEqual(main.func_1(10), 10)
+        self.assertEqual(self.pc.func_1(10), 10)
 
     #def test_func_1_fail(self) -> None:
     #    """ func_1, fail """
-    #    self.assertEqual(main.func_1(10), 20)
+    #    self.assertEqual(self.pc.func_1(10), 20)
 
     def test_func_2_success(self) -> None:
         """ func_2, success """
-        self.assertEqual(main.func_2(10), 10)
+        self.assertEqual(self.pc.func_2(10), 10)
 
     #def test_func_2_fail(self) -> None:
     #    """ func_2, fail """
-    #    self.assertEqual(main.func_2(10), 20)
+    #    self.assertEqual(self.pc.func_2(10), 20)
